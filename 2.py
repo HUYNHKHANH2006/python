de7f137ae38d85287ecdf1aa0daaf6d57072f0f2
HUYNHKHANH2006 <khanhhuynhtrai@gmail.com>
2026-04-17 09:23:12 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,10 @@
 # Nhập chiều dài, chiều rộng và chiều cao của hình khối chữ nhật
-length = int(input("Nhập chiều dài đáy hình chữ nhật (cm):>? "))
-width = int(input("Nhập chiều rộng đáy hình chữ nhật (cm):>? "))
-height = int(input("Nhập chiều cao hình khối chữ nhật (cm):>? "))
+length = int(input("nhap chieu dai day hinh chu nhat (cm):>? "))
+width = int(input("nhap chieu rong day hinh chu nhat (cm):>? "))
+height = int(input("nhap chieu cao hinh khoi chu nhat (cm):>? "))
 
 # Tính và xuất kết quả
 area = length * width
 volume = area * height
-print(f"Diện tích đáy hình chữ nhật = {area}cm²")
-print(f"Thể tích hình khối = {volume}cm³")
+print(f"Diện tích đáy hình chữ nhật = {area}")
+print(f"Thể tích hình khối = {volume}")
